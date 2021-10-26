4926ac79ef0311f396cdcf434f57b696764a6696
R. Indira Valentina Réquiz Molina <valentinarequiz@gmail.com>
2021-10-26 19:49:29 -0300

    actividad de clase

--- ProgramacionImperativa/app.js
+++ ProgramacionImperativa/app.js
@@ -4,3 +4,30 @@ console.log("Hola");
 
 const miPrimerVariableString = "¡Hola, Mundo!";
 console.log(miPrimerVariableString);
+
+const nombre= "Valentina"
+
+console.log ("Hola, " + nombre);
+
+// Ejercicio de mesa de trabajo
+
+// actividad de la vida cotidiana 1 - Tarea de Matemática
+
+const numero_1 = 2
+const numero_2 = 5
+const suma = numero_1 + numero_2
+console.log("El resultado de la suma es: " + suma);
+
+// actividad cotidiana 2 - 
+
+const ingrediente1="leche";
+const i1cantidad= 1;
+
+const ingrediente2= "chocolate";
+const i2cantidad= 3;
+
+console.log("La receta para la chocolatada es: 1- " +ingrediente1 + " "  + i1cantidad + " " + "vaso" + ", 2- " + ingrediente2 + " " + i2cantidad + " "  +"cucharadas");
+
+
+
+
