2a53660ebc229e28459c290c23942e7f9db7a2a2
R. Indira Valentina Réquiz Molina <valentinarequiz@gmail.com>
2021-10-27 14:15:42 -0300

    ejercicio del Viejo MAcDonald

---
+++ ProgramacionImperativa/funciones/app.js
@@ -0,0 +1,21 @@
+function chorus (animal, sonido){
+const texto =
+"Oh, Mc Donald saraza tenia un " +
+ animal + 
+" que hace "  +
+ sonido + 
+ ", " + 
+ sonido +
+  ", " + 
+sonido;
+
+return texto;
+}
+
+
+console.log(chorus("cerdo","oink"));
+console.log(chorus("vaca","muuu"));
+
+
+// Actividad en mesa de trabajo - clase 5
+
