82669106890ce4a28e8f8b38f8243831c22f3f4a
R. Indira Valentina Réquiz Molina <valentinarequiz@gmail.com>
2021-11-10 13:39:42 -0300

    arrays y métodos

--- content/bimestre1/progimperativa/proyectosprogimp/arraysystrings.js
+++ content/bimestre1/progimperativa/proyectosprogimp/arraysystrings.js
@@ -63,7 +63,7 @@ function menciona(texto, palabra){
 console.log(menciona(texto, palabra))
 
 //5
-/*eniendo como punto de partida la cadena de texto '¡Hola!, soy Carli', deberemos recortarla para poder obtener el nombre 'Carli' en una nueva variable llamada licenciada.
+/*Teniendo como punto de partida la cadena de texto '¡Hola!, soy Carli', deberemos recortarla para poder obtener el nombre 'Carli' en una nueva variable llamada licenciada.
 
 IMPORTANTE: Para la resolución tendremos que usar la función slice().*/
 
